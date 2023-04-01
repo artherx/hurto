cf0422f3e396ae43825757c9aad6745b3838e922
artherx <xxartherxx@gmail.com>
2023-04-01 17:13:31 -0500

    añadi firebase y bordes para el area de inicio de secion

--- app/src/main/java/com/example/hurto/MainActivity.kt
+++ app/src/main/java/com/example/hurto/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.hurto
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var  auth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
