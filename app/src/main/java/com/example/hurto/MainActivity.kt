cdad3b03452088627d38ab7f9638364cf88fa11f
artherx <xxartherxx@gmail.com>
2023-04-02 10:41:48 -0500

    añadi firebase y se implementa en el xml para ir a iniciar secion o ir a registrar, que recuerda si inreso un usuario y lo coloca en el otro frame

--- app/src/main/java/com/example/hurto/MainActivity.kt
+++ app/src/main/java/com/example/hurto/MainActivity.kt
@@ -3,30 +3,62 @@ package com.example.hurto
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
 import com.google.firebase.auth.FirebaseAuth
 
+
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var  auth: FirebaseAuth
-
-
+    private lateinit var auth:FirebaseAuth
+    private lateinit var stateListener:FirebaseAuth.AuthStateListener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
         val btnRegis = findViewById<AppCompatButton>(R.id.btnRegis)
         val etUser = findViewById<AppCompatEditText>(R.id.usuario)
         val etPass = findViewById<AppCompatEditText>(R.id.password)
+        val txtPassRe = findViewById<TextView>(R.id.passSingh)
+        val name:String = intent.extras?.getString("recordarI").orEmpty()
+        etUser.setText(name)
+
 
         btnRegis.setOnClickListener {
             val name = etUser.text.toString()
+            val pass = etPass.text.toString()
             if (name.isNotEmpty()){
-                val intent = Intent(this,HomeActivity::class.java)
-                intent.putExtra("Extra name",name)
-                startActivity(intent)
+                if (pass.isNotEmpty()){
+                    signIn(name,pass)
+                    val intent = Intent(this,HomeActivity::class.java)
+                    intent.putExtra("Extra name",name)
+                    startActivity(intent)
+                }
             }
 
         }
+        txtPassRe.setOnClickListener {
+            val nameR = etUser.text.toString()
+            val intent = Intent(this, RegisActivity::class.java)
+            if (nameR.isNotEmpty()){
+                intent.putExtra("recordarR", nameR)
+            }
+            startActivity(intent)
+        }
+    }
+
+    private fun signIn(email:String,password:String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) {task->
+                if (task.isSuccessful){
+                    val user = auth.currentUser
+                    Toast.makeText(baseContext,"signInWithEmail:success", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    Toast.makeText(baseContext,"Authentication failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
